0513e4a6fb7fec13500f85e778836ca82636605e
SeolhwaYoo <sh.yoo@dypic.co.kr>
2023-10-10 15:48:12 +0900

    edit

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
-const head=document.querySelector('.con-wrap__header2');
+const head = document.querySelector('.con-wrap__header2');
 
-head.innerHTML=`
+head.innerHTML = `
 <div class="inner">
             <div class="d-flex header2__pc-wrap">
                 <h1>
@@ -83,47 +83,109 @@ head.innerHTML=`
 `
 //nav_2
 //pc
-const nav2_main=document.querySelectorAll('.main-list__menu');
-const nav2_mainlist=document.querySelector('.main-list');
-const logo=document.querySelector('.logo');
-const header_right=document.querySelector('.pc-wrap__right a');
+const nav2_main = document.querySelectorAll('.main-list__menu');
+const nav2_mainlist = document.querySelector('.main-list');
+const logo = document.querySelector('.logo');
+const header_right = document.querySelectorAll('.pc-wrap__right a');
 
 nav2_main.forEach(el => {
-    el.addEventListener('focus',function(){
+    el.addEventListener('focus', function () {
         nav2_mainlist.classList.add('active');
     })
 });
-logo.addEventListener('focus',function(){
+logo.addEventListener('focus', function () {
     nav2_mainlist.classList.remove('active');
 });
-header_right.addEventListener('focus',function(){
+header_right[0].addEventListener('focus', function () {
     nav2_mainlist.classList.remove('active');
 });
+
+window.addEventListener('scroll', function () {
+    const prevScroll = window.scrollY;
+    const visualSecHeight = document.querySelector('.visual-con').clientHeight - 50;
+    // console.log(visualSecHeight);
+
+    if (prevScroll > 0 && prevScroll <= visualSecHeight) {
+        gsap.to(head, {
+            duration: 0.3,
+            backgroundColor: 'rgba(255,255,255,0.8)',
+        })
+        gsap.to(logo,{
+            duration:0.3,
+            color:'#3066E2',
+        })
+        nav2_main.forEach(el=>{
+            gsap.to(el,{
+                duration:0.3,
+                color:'#3066E2',
+            })
+        })
+        gsap.to(header_right[0],{
+            duration:0.3,
+            backgroundColor:'#3066E2',
+            color:'#fff',
+        })
+        gsap.to(header_right[1],{
+            duration:0.3,
+            color:'#3066E2',
+        })
+
+    } else if (prevScroll > visualSecHeight) {
+        gsap.to(head, {
+            duration: 0.3,
+            backgroundColor: '#3066E2',
+        })
+        gsap.to(logo,{
+            duration:0.3,
+            color:'#fff',
+        })
+        nav2_main.forEach(el=>{
+            gsap.to(el,{
+                duration:0.3,
+                color:'#fff',
+            })
+        })
+        gsap.to(header_right[0],{
+            duration:0.3,
+            backgroundColor:'#fff',
+            color:'#3066E2',
+        })
+        gsap.to(header_right[1],{
+            duration:0.3,
+            color:'#fff',
+        })
+    } else {
+        gsap.to(head, {
+            duration: 0.3,
+            backgroundColor: '#fff',
+        })
+    }
+})
 //mobile
-const nav2_open=document.querySelector('.nav-style-2 .mo-nav-open');
-const nav2_close=document.querySelector('.mo-nav-wrap-2 .mo-nav-close');
-const nav2_moWrap=document.querySelector('.mo-nav-wrap-2');
-const nav2_subList=document.querySelectorAll('.mo-nav-wrap-2 .sub-list');
+const nav2_open = document.querySelector('.nav-style-2 .mo-nav-open');
+const nav2_close = document.querySelector('.mo-nav-wrap-2 .mo-nav-close');
+const nav2_moWrap = document.querySelector('.mo-nav-wrap-2');
+const nav2_subList = document.querySelectorAll('.mo-nav-wrap-2 .sub-list');
 
-nav2_open.addEventListener('click',function(){
+nav2_open.addEventListener('click', function () {
     nav2_moWrap.classList.add('active');
-    document.querySelector('body').style.overflowY='hidden';
+    document.querySelector('body').style.overflowY = 'hidden';
 })
-nav2_close.addEventListener('click',function(){
+nav2_close.addEventListener('click', function () {
     nav2_moWrap.classList.remove('active');
-    document.querySelector('body').style.overflowY='auto';
+    document.querySelector('body').style.overflowY = 'auto';
 })
 
-nav2_subList.forEach(sub=>{
-    sub.previousElementSibling.addEventListener('click',function(e){
+nav2_subList.forEach(sub => {
+    sub.previousElementSibling.addEventListener('click', function (e) {
         e.preventDefault();
-        if (!this.classList.contains('active')){
-            sub.style.height=(sub.children[0].offsetHeight * sub.children.length)+'px';
+        if (!this.classList.contains('active')) {
+            sub.style.height = (sub.children[0].offsetHeight * sub.children.length) + 'px';
             this.classList.add('active');
-        }else{
-            sub.style.height=0;
+        } else {
+            sub.style.height = 0;
             this.classList.remove('active');
         }
-        
+
     })
 })
--- js/index.js
+++ js/index.js
@@ -2,22 +2,53 @@
 const graph = document.querySelectorAll('.graph-stick');
 const graphText = document.querySelectorAll('.graph-percent span');
 const percent = [80, 95, 80, 90, 95];
-const shame = [3240, 95, 2890, 12062, 2860];
-// const shame=[162.0, 4.25, 144.5,603.1, 143.0];
 let i = 0;
 let j = 0;
 let isPlay;
 
+//4th section
 let swiperState;
+let swiper = new Swiper(".mySwiper", {
+    speed: 5000,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    loopAdditionalSlides: 1,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 2,
+        },
+        1400: {
+            slidesPerView: 4,
+        }
+    }
+});
+swiper.autoplay.stop();
 
 const reviewCon = document.querySelector('.review-con');
 const reviewTitle = document.querySelector('.review-con .review-title');
+const slideList = document.querySelector('.review-wrap');
 const slideItem = document.querySelectorAll('.review-box');
 const itemWidth = (slideItem[0].offsetWidth) + parseInt(window.getComputedStyle(slideItem[0]).getPropertyValue('margin-right'));
 
-//초기 css세팅
-slideItem.forEach((el, index) => {
-    el.style.transform = `translateX(${itemWidth * (slideItem.length - index)}px)`;
+//5th section
+const iconAcd = document.querySelectorAll('.accordion .accordion-item');
+const iconAcdBtn = document.querySelectorAll('.accordion .accordion-head');
+const iconAcdBody = document.querySelectorAll('.accordion .accordion-body');
+
+const noIconAcd = document.querySelectorAll('.accordion-simple .accordion-item');
+const noIconAcdBtn = document.querySelectorAll('.accordion-simple .accordion-head');
+const noIconAcdBody = document.querySelectorAll('.accordion-simple .accordion-body');
+
+const acdText = document.querySelectorAll('.accordion-text');
+const acdBody = document.querySelectorAll('.accordion-body');
+
+acdBody.forEach((item, index) => {
+    item.style.height = acdText[index].offsetHeight + 'px';
 });
 
 function makeChart() {
@@ -34,86 +65,103 @@ function makeChart() {
 
             }, 20);
         })
-        // graphText.forEach((el, index) => {
-        //     const chartTextAct = setInterval(() => {
-        //         if (j <= shame[index]) {
-        //             el.innerHTML = j;
-        //             j++;
-        //         }else{
-        //             clearInterval(chartTextAct);
-        //         }
-        //     }, 20);
-        // })
 
     }
 }
 
+//3rd section counter
+// CONFIG
+let visibilityIds = ['#counters_1', '#counters_2', '#counters_3']; //must be an array, could have only one element
+let counterClass = '.counter';
+let defaultSpeed = 3000; //default value
+//config end
+
+function getVisibilityStatus() {
+    elValFromTop = [];
+    var windowHeight = $(window).height(),
+        windowScrollValFromTop = $(this).scrollTop();
+
+    visibilityIds.forEach(function (item, index) { //Call each class
+        try { //avoid error if class not exist
+            elValFromTop[index] = Math.ceil($(item).offset().top);
+        } catch (err) {
+            return;
+        }
+        // if the sum of the window height and scroll distance from the top is greater than the target element's distance from the top, 
+        //it should be in view and the event should fire, otherwise reverse any previously applied methods
+        if ((windowHeight + windowScrollValFromTop) > elValFromTop[index]) {
+            counter_init(item);
+        }
+    });
+}
+
+function counter_init(groupId) {
+    let num, speed, direction, index = 0;
+    $(counterClass).each(function () {
+        num = $(this).attr('data-TargetNum');
+        speed = $(this).attr('data-Speed');
+        direction = $(this).attr('data-Direction');
+        easing = $(this).attr('data-Easing');
+        if (speed == undefined) speed = defaultSpeed;
+        $(this).addClass('c_' + index); //add a class to recognize each counter
+        doCount(num, index, speed, groupId, direction, easing);
+        index++;
+    });
+}
+
+function doCount(num, index, speed, groupClass, direction, easing) {
+    let className = groupClass + ' ' + counterClass + '.' + 'c_' + index;
+    if (easing == undefined) easing = "swing";
+    $(className).animate({
+        num
+    }, {
+        duration: +speed,
+        easing: easing,
+        step: function (now) {
+            if (direction == 'reverse') {
+                $(this).text(num - Math.floor(now));
+            } else {
+                $(this).text(Math.floor(now));
+            }
+        },
+        complete: doCount
+    });
+}
+//counter end
+
 window.addEventListener('scroll', function () {
     const scrollPoint = (window.innerHeight / 10) * 9;
     const reviewScroll = reviewTitle.getBoundingClientRect().top;
+
     //3rd section
     if (graph[0].getBoundingClientRect().top <= scrollPoint) {
-
         setTimeout(makeChart, 600);
+        getVisibilityStatus();
     }
 
     //4th section
     if (reviewScroll <= scrollPoint) {
         let tl = gsap.timeline();
 
-        tl.to(reviewTitle, {
-            duration: 0.3,
-            opacity: 1,
-            y: 0,
-        })
-
-        slideItem.forEach(el => {
-            gsap.to(el, {
-                duration: 1,
-                x: 0,
-            })
-        })
-
-        //swiper js
+        // tl.to(reviewTitle, {
+        //     duration: 0.3,
+        //     opacity: 1,
+        //     y: 0,
+        // })
         if (!swiperState) {
             swiperState = true;
-            swiper = new Swiper(".mySwiper", {
-                speed: 5000,
-                slidesPerView: 1,
-                spaceBetween: 20,
-                loop: true,
-                loopAdditionalSlides: 1,
-                autoplay: {
-                    delay: 0,
-                    disableOnInteraction: false,
-                },
-                breakpoints: {
-                    1024: {
-                        slidesPerView: 2,
-                    },
-                    1400: {
-                        slidesPerView: 4,
-                    }
-                }
-            });
+            slideItem.forEach(el => {
+                tl.to(el, {
+                    duration: 0.2,
+                    opacity: 1,
+                })
+            })
+            //swiper js
+            setTimeout(() => {
+                swiper.autoplay.start();
+            }, 300);
         }
     }
-});
-
-//5th section
-const iconAcd = document.querySelectorAll('.accordion .accordion-item');
-const iconAcdBtn = document.querySelectorAll('.accordion .accordion-head');
-const iconAcdBody = document.querySelectorAll('.accordion .accordion-body');
-
-const noIconAcd = document.querySelectorAll('.accordion-simple .accordion-item');
-const noIconAcdBtn = document.querySelectorAll('.accordion-simple .accordion-head');
-const noIconAcdBody = document.querySelectorAll('.accordion-simple .accordion-body');
-
-const acdText = document.querySelectorAll('.accordion-text');
-const acdBody = document.querySelectorAll('.accordion-body');
-
-acdBody.forEach((item, index) => {
-    item.style.height = acdText[index].offsetHeight + 'px';
 });
 
 window.addEventListener('resize', function () {
