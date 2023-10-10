5946a51f76c524d7dcbad7e8d2752a5b32c174f7
SeolhwaYoo <sh.yoo@dypic.co.kr>
2023-10-10 09:36:26 +0900

    sum

--- js/index.js
+++ js/index.js
@@ -8,6 +8,18 @@ let i = 0;
 let j = 0;
 let isPlay;
 
+let swiperState;
+
+const reviewCon = document.querySelector('.review-con');
+const reviewTitle = document.querySelector('.review-con .review-title');
+const slideItem = document.querySelectorAll('.review-box');
+const itemWidth = (slideItem[0].offsetWidth) + parseInt(window.getComputedStyle(slideItem[0]).getPropertyValue('margin-right'));
+
+//초기 css세팅
+slideItem.forEach((el, index) => {
+    el.style.transform = `translateX(${itemWidth * (slideItem.length - index)}px)`;
+});
+
 function makeChart() {
     if (!isPlay) {
         isPlay = true;
@@ -38,10 +50,53 @@ function makeChart() {
 
 window.addEventListener('scroll', function () {
     const scrollPoint = (window.innerHeight / 10) * 9;
-
+    const reviewScroll = reviewTitle.getBoundingClientRect().top;
+    //3rd section
     if (graph[0].getBoundingClientRect().top <= scrollPoint) {
 
         setTimeout(makeChart, 600);
+    }
+
+    //4th section
+    if (reviewScroll <= scrollPoint) {
+        let tl = gsap.timeline();
+
+        tl.to(reviewTitle, {
+            duration: 0.3,
+            opacity: 1,
+            y: 0,
+        })
+
+        slideItem.forEach(el => {
+            gsap.to(el, {
+                duration: 1,
+                x: 0,
+            })
+        })
+
+        //swiper js
+        if (!swiperState) {
+            swiperState = true;
+            swiper = new Swiper(".mySwiper", {
+                speed: 5000,
+                slidesPerView: 1,
+                spaceBetween: 20,
+                loop: true,
+                loopAdditionalSlides: 1,
+                autoplay: {
+                    delay: 0,
+                    disableOnInteraction: false,
+                },
+                breakpoints: {
+                    1024: {
+                        slidesPerView: 2,
+                    },
+                    1400: {
+                        slidesPerView: 4,
+                    }
+                }
+            });
+        }
     }
 });
 
@@ -50,33 +105,33 @@ const iconAcd = document.querySelectorAll('.accordion .accordion-item');
 const iconAcdBtn = document.querySelectorAll('.accordion .accordion-head');
 const iconAcdBody = document.querySelectorAll('.accordion .accordion-body');
 
-const noIconAcd=document.querySelectorAll('.accordion-simple .accordion-item');
-const noIconAcdBtn=document.querySelectorAll('.accordion-simple .accordion-head');
-const noIconAcdBody=document.querySelectorAll('.accordion-simple .accordion-body');
+const noIconAcd = document.querySelectorAll('.accordion-simple .accordion-item');
+const noIconAcdBtn = document.querySelectorAll('.accordion-simple .accordion-head');
+const noIconAcdBody = document.querySelectorAll('.accordion-simple .accordion-body');
 
 const acdText = document.querySelectorAll('.accordion-text');
-const acdBody= document.querySelectorAll('.accordion-body');
+const acdBody = document.querySelectorAll('.accordion-body');
 
-acdBody.forEach((item,index) => {
-    item.style.height=acdText[index].offsetHeight+'px';
+acdBody.forEach((item, index) => {
+    item.style.height = acdText[index].offsetHeight + 'px';
 });
 
-window.addEventListener('resize',function(){
-    acdBody.forEach((item,index) => {
-        item.style.height=acdText[index].offsetHeight+'px';
+window.addEventListener('resize', function () {
+    acdBody.forEach((item, index) => {
+        item.style.height = acdText[index].offsetHeight + 'px';
     });
 })
 accordionAct(iconAcdBtn, iconAcd, iconAcdBody);
 accordionAct(noIconAcdBtn, noIconAcd, noIconAcdBody);
 
-function accordionAct(btn, item, body){
+function accordionAct(btn, item, body) {
     btn.forEach((btn, index) => {
         btn.addEventListener('click', function () {
             if (item[index].classList.contains('active')) {
                 item.forEach(all => {
                     all.classList.remove('active');
                 })
-                body.forEach(all=>{
+                body.forEach(all => {
                     all.classList.add('none');
                 })
             } else {
@@ -84,7 +139,7 @@ function accordionAct(btn, item, body){
                     all.classList.remove('active');
                 })
                 item[index].classList.add('active');
-                body.forEach(all=>{
+                body.forEach(all => {
                     all.classList.add('none');
                 })
                 body[index].classList.remove('none');
@@ -94,31 +149,31 @@ function accordionAct(btn, item, body){
 }
 
 //gsap
-const visual_title=document.querySelectorAll('.visual__hid .hid__span');
-const visual_btn=document.querySelectorAll('.visual-start a');
+const visual_title = document.querySelectorAll('.visual__hid .hid__span');
+const visual_btn = document.querySelectorAll('.visual-start a');
 
-const service_title=document.querySelector('.service__title');
+const service_title = document.querySelector('.service__title');
 
 //1st section gsap
-function visual(){
-    let tl=gsap.timeline();
+function visual() {
+    let tl = gsap.timeline();
 
-    tl.to(visual_title[0],{
-        duration:0.5,
-        y:0,
-    },'-=30%').to(visual_title[1],{
-        duration:0.5,
-        y:0,
-    },'-=30%').to(visual_title[2],{
-        duration:0.5,
-        y:0,
-    },'-=30%').to(visual_btn[0],{
-        duration:0.5,
-        y:0,
-    },'-=30%').to(visual_btn[1],{
-        duration:0.5,
-        y:0,
-    },'-=30%')
+    tl.to(visual_title[0], {
+        duration: 0.5,
+        y: 0,
+    }, '-=30%').to(visual_title[1], {
+        duration: 0.5,
+        y: 0,
+    }, '-=30%').to(visual_title[2], {
+        duration: 0.5,
+        y: 0,
+    }, '-=30%').to(visual_btn[0], {
+        duration: 0.5,
+        y: 0,
+    }, '-=30%').to(visual_btn[1], {
+        duration: 0.5,
+        y: 0,
+    }, '-=30%')
 }
 visual();
 
