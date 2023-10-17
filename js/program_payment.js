7ac415b48bc209214fef507560934b8be1ec0478
SeolhwaYoo <sh.yoo@dypic.co.kr>
2023-10-17 18:28:49 +0900

    프로그램 결제페이지

---
+++ js/program_payment.js
@@ -0,0 +1,58 @@
+//header setting
+const listIndex = 1;
+const pageIndex = 0;
+
+//pc
+const gnbList = document.querySelector('.gnb-box__gnb-list');
+
+gnbList.children[listIndex].classList.add('active');
+subList[listIndex].children[pageIndex].children[0].classList.add('active');
+
+const activeSubList = document.querySelector('.gnb-box__gnb-list .active .gnb-list__sub-list');
+const j = activeSubList.childElementCount;
+
+activeSubList.style.height = subHeight * j + 'px';
+
+//mobile
+const moGnbList = document.querySelector('.gnb-box__mo-gnb-list');
+
+moGnbList.children[listIndex].classList.add('active');
+moSubList[listIndex].children[pageIndex].children[0].classList.add('active');
+
+const activeMoSubList = document.querySelector('.gnb-box__mo-gnb-list .active .gnb-list__mo-sub-list');
+const j2 = activeMoSubList.childElementCount;
+
+activeMoSubList.style.height = moSubHeight * j2 + 'px';
+//header setting end
+
+//main
+const navBtn = document.querySelectorAll('.nav-tabs .nav-link');
+const textBox = document.querySelectorAll('.nav-tabs .tab-box');
+
+navBtn.forEach((btn, index) => {
+    btn.addEventListener('click', function () {
+        navBtn.forEach(all => {
+            all.classList.remove('active');
+        })
+        this.classList.add('active');
+        textBox.forEach(all => {
+            all.classList.remove('active');
+        })
+        textBox[index].classList.add('active');
+    });
+});
+
+const select = document.querySelectorAll('.text-change-select');
+const preText = document.querySelectorAll('.pre-box');
+
+select.forEach((el, index) => {
+    el.addEventListener('change', function () {
+        const val = el.options[el.selectedIndex].value;
+        const child = preText[index].children;
+
+        for (let i = 0; i < child.length; i++) {
+            child[i].classList.remove('active');
+        }
+        preText[index].children[val].classList.add('active');
+    })
+})
